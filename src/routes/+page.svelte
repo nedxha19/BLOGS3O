de97801749622818649c09c31870ed71e043d149
Nedit <nedxha19@htl-shkoder.com>
2026-03-04 12:28:04 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import device from '$lib/assets/device.jpg';
+</script>
+
+<header class="h-[500px] bg-center bg-cover" style="background-image: url(device);"> 
+<h1 class="text-5xl">Electronic Devices</h1>
+</header>
+
+<main>
+
+</main>
+ 
+
+<footer>
+
+
+</footer>
